a248ac454802a1155ab1a8a36300512c087f3f77
Sanjeev Kumar <sanjeevkr1314@gmail.com>
2023-07-13 18:09:21 +0530

    Add environment files

---
+++ app.js
@@ -0,0 +1,89 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+// connect to mongodb database
+mongoose
+.connect("mongodb://127.0.0.1:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then((success, err) => {
+    if (success) {
+        console.log(`CONNECTED TO MONGO!`);
+    } else {
+        console.log(`OH NO! MONGO CONNECTION ERROR!`);
+        console.log(err);
+    }
+});
+
+// creating schema
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+// creating a model
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    
+    newUser.save()
+    .then((success, err)=>{
+        if(success){
+            res.render("secrets");
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+app.post("/login", function(req, res){
+    const username= req.body.username;
+    const password= req.body.password;
+
+    User.findOne({email: username})
+    .then((foundUser, err)=>{
+        if(foundUser){
+            if(foundUser.password === password){
+                res.render("secrets");
+            }else{
+                res.send("Incorrect Password!")
+            }
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000.");
+});
